7cf52ec36b82b6f7b726eb1c81eec9035214f096
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-07-08 15:13:35 +0100

    another commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,10 +4,7 @@
         <div class="modal fade" id="LoginModal" tabindex="-1" role="dialog" aria-labelledby="LoginModal" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
-
                 <div class="modal-body">
-
-
                         <ul class="nav nav-fill nav-pills mb-3" id="pills-tab" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
@@ -16,10 +13,8 @@
                                 <a class="nav-link" id="pills-register-tab" data-toggle="pill" href="#pills-register" role="tab" aria-controls="pills-register" aria-selected="false">Signup</a>
                             </li>
                         </ul>
-
                         <div class="tab-content" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="pills-login-tab">
-                            
+                        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="pills-login-tab"> 
                             <h5 class="text-center">Login Please</h5>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
@@ -30,16 +25,12 @@
                                 <label for="exampleInputPassword1">Password</label>
                                 <input type="password" @keyup.enter="login" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
-
                              <div class="form-group">
                                 <button class="btn btn-primary" @click="login">Login</button>
                             </div>
-
                         </div>
-                        <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="pills-register-tab">
-                            
-                             <h5 class="text-center">Create New Account</h5>
-                             
+                        <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="pills-register-tab">                     
+                            <h5 class="text-center">Create New Account</h5>     
                             <div class="form-group">
                                 <label for="name">Your name</label>
                                 <input type="text" v-model="name" class="form-control" id="name" placeholder="Your nice name">
@@ -53,13 +44,12 @@
                                 <label for="password">Password</label>
                                 <input type="password" v-model="password" class="form-control" id="password" placeholder="Password">
                             </div>
-
                             <div class="form-group">
                                 <button class="btn btn-primary" @click="register">Signup</button>
                             </div>
 
                         </div>
-                        </div>
+                    </div>
                     
  
                 </div>
